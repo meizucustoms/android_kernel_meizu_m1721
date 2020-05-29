5458d12eecd1c8d169a69ece2b893c2ff4174c09
RomanDesigner <romashkarus08@gmail.com>
2020-05-29 16:19:57 +0300

    m1721: use Moto's cirrus playback driver

--- include/uapi/sound/msm-cirrus-playback.h
+++ include/uapi/sound/msm-cirrus-playback.h
@@ -11,12 +11,17 @@
  */
 #ifndef _UAPI_MSM_CIRRUS_SPK_PR_H
 #define _UAPI_MSM_CIRRUS_SPK_PR_H
+
 #include <linux/types.h>
 #include <linux/ioctl.h>
+
+
 #define CIRRUS_SP			0x10027053
 #define CIRRUS_SP_ENABLE		0x10002001
+
 #define CRUS_MODULE_ID_TX		0x00000002
 #define CRUS_MODULE_ID_RX		0x00000001
+
 /*
  * CRUS_PARAM_RX/TX_SET_USECASE
  * 0 = Music Playback in firmware
@@ -59,12 +64,30 @@
  */
 #define CRUS_PARAM_RX_GET_TEMP		0x00A1AF07
 #define CRUS_PARAM_TX_GET_TEMP_CAL	0x00A1BF06
+/*
+ * CRUS_PARAM_RX_SET_DELTA_CONFIG
+ * load seamless transition config string
+ *
+ * CRUS_PARAM_RX_RUN_DELTA_CONFIG
+ * execute the loaded seamless transition
+ */
+#define CRUS_PARAM_RX_SET_DELTA_CONFIG	0x00A1AF0D
+#define CRUS_PARAM_RX_RUN_DELTA_CONFIG	0x00A1AF0E
+/*
+ * CRUS_PARAM_RX_CHANNEL_SWAP
+ * initiate l/r channel swap transition
+ */
+#define CRUS_PARAM_RX_CHANNEL_SWAP	0x00A1AF12
+
 #define CRUS_AFE_PARAM_ID_ENABLE	0x00010203
+
 #define SPK_PROT_IOCTL_MAGIC		'a'
+
 #define CRUS_SP_IOCTL_GET	_IOWR(SPK_PROT_IOCTL_MAGIC, 219, void *)
 #define CRUS_SP_IOCTL_SET	_IOWR(SPK_PROT_IOCTL_MAGIC, 220, void *)
 #define CRUS_SP_IOCTL_GET_CALIB	_IOWR(SPK_PROT_IOCTL_MAGIC, 221, void *)
 #define CRUS_SP_IOCTL_SET_CALIB	_IOWR(SPK_PROT_IOCTL_MAGIC, 222, void *)
+
 #define CRUS_SP_IOCTL_GET32		_IOWR(SPK_PROT_IOCTL_MAGIC, 219, \
 	compat_uptr_t)
 #define CRUS_SP_IOCTL_SET32		_IOWR(SPK_PROT_IOCTL_MAGIC, 220, \
@@ -73,6 +96,7 @@
 	compat_uptr_t)
 #define CRUS_SP_IOCTL_SET_CALIB32	_IOWR(SPK_PROT_IOCTL_MAGIC, 222, \
 	compat_uptr_t)
+
 struct crus_sp_ioctl_header {
 	uint32_t size;
 	uint32_t module_id;
@@ -80,4 +104,5 @@ struct crus_sp_ioctl_header {
 	uint32_t data_length;
 	void *data;
 };
+
 #endif /* _UAPI_MSM_CIRRUS_SPK_PR_H */
--- sound/soc/msm/qdsp6v2/msm-cirrus-playback.h
+++ sound/soc/msm/qdsp6v2/msm-cirrus-playback.h
@@ -11,28 +11,33 @@
  */
 #ifndef MSM_CIRRUS_PLAYBACK_H
 #define MSM_CIRRUS_PLAYBACK_H
+
 #include <linux/slab.h>
 #include <sound/soc.h>
 #include <sound/apr_audio-v2.h>
 #include <sound/q6afe-v2.h>
 #include <sound/q6audio-v2.h>
 #include <uapi/sound/msm-cirrus-playback.h>
+
 struct afe_custom_crus_set_config_t {
 	struct apr_hdr hdr;
 	struct afe_port_cmd_set_param_v2 param;
 	struct afe_port_param_data_v2 data;
 } __packed;
+
 struct afe_custom_crus_get_config_t {
 	struct apr_hdr hdr;
 	struct afe_port_cmd_get_param_v2 param;
 	struct afe_port_param_data_v2 data;
 } __packed;
+
 /* Payload struct for getting or setting one integer value from/to the DSP
  * module
  */
 struct crus_single_data_t {
-	int32_t value;
+	int32_t	value;
 };
+
 /* Payload struct for getting or setting two integer values from/to the DSP
  * module
  */
@@ -40,6 +45,7 @@ struct crus_dual_data_t {
 	int32_t data1;
 	int32_t data2;
 };
+
 /* Payload struct for getting or setting three integer values from/to the DSP
  * module
  */
@@ -48,6 +54,7 @@ struct crus_triple_data_t {
 	int32_t data2;
 	int32_t data3;
 };
+
 /* Payload struct for setting the RX and TX use cases */
 struct crus_rx_run_case_ctrl_t {
 	int32_t value;
@@ -59,6 +66,7 @@ struct crus_rx_run_case_ctrl_t {
 	int32_t z_r;
 	int32_t atemp;
 };
+
 /* Payload struct for getting calibration result from DSP module */
 struct cirrus_cal_result_t {
 	int32_t status_l;
@@ -67,21 +75,42 @@ struct cirrus_cal_result_t {
 	int32_t status_r;
 	int32_t checksum_r;
 	int32_t z_r;
-	int32_t atemp;
 };
-/* Payload struct for sending an external configuration string to the DSP module
+
+#define APR_CHUNK_SIZE		256
+#define CONFIG_FILE_SIZE	128
+#define PAYLOAD_FOLLOWS_CONFIG	4
+#define MAX_TUNING_CONFIGS	4
+#define MIN_CHAN_SWAP_SAMPLES	48
+#define MAX_CHAN_SWAP_SAMPLES	9600
+
+/* Payload struct for sending an external configuration string to the DSP
+ * module
  */
 struct crus_external_config_t {
 	uint32_t total_size;
 	uint32_t chunk_size;
 	int32_t done;
-	const char *config;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
 };
-int afe_apr_send_pkt_crus(void *data, int index, int set);
-int crus_afe_get_param(int port, int module, int param, int length, void *data);
-int crus_afe_set_param(int port, int module, int param, int data_size,
-		       void *data_ptr);
-int crus_afe_send_config(const char *string, int32_t module);
-int crus_afe_callback(void *payload, int size);
+
+/* Payload struct for sending an external tuning transition string to the DSP
+ * module
+ */
+struct crus_delta_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	int32_t done;
+	int32_t index;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
+};
+
+extern int afe_apr_send_pkt_crus(void *data, int index, int set);
+extern int crus_afe_callback(void *payload, int size);
 void msm_crus_pb_add_controls(struct snd_soc_platform *platform);
+
 #endif /* _MSM_CIRRUS_PLAYBACK_H */
