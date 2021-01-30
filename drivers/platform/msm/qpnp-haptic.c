04ec4bc50d6cfc4e9f4eed8f89e0c82c64810731
Roman Rihter <romashkarus08@gmail.com>
2021-01-30 22:41:28 +0300

    m1721: fix vibration strength

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -318,9 +318,6 @@ struct qpnp_hap {
 	struct qpnp_pwm_info pwm_info;
 	struct mutex lock;
 	struct mutex wf_lock;
-	struct mutex set_lock;
-	spinlock_t td_lock;
-	struct work_struct td_work;
 	struct completion completion;
 	enum qpnp_hap_mode play_mode;
 	enum qpnp_hap_auto_res_mode auto_res_mode;
@@ -333,9 +330,6 @@ struct qpnp_hap {
 	u32 timeout_ms;
 	u32 time_required_to_generate_back_emf_us;
 	u32 vmax_mv;
-	u32 vtg_min;
-	u32 vtg_max;
-	u32 vtg_default;
 	u32 ilim_ma;
 	u32 sc_deb_cycles;
 	u32 int_pwm_freq_khz;
@@ -369,7 +363,6 @@ struct qpnp_hap {
 	bool correct_lra_drive_freq;
 	bool misc_trim_error_rc19p2_clk_reg_present;
 	bool perform_lra_auto_resonance_search;
-	int td_value;
 };
 
 static struct qpnp_hap *ghap;
@@ -735,16 +728,16 @@ static int qpnp_hap_vmax_config(struct qpnp_hap *hap)
 	u8 reg = 0;
 	int rc, temp;
 
-	if (hap->vmax_mv < hap->vtg_min)
-		hap->vmax_mv = hap->vtg_min;
-	else if (hap->vmax_mv > hap->vtg_max)
-		hap->vmax_mv = hap->vtg_max;
+	if (hap->vmax_mv < QPNP_HAP_VMAX_MIN_MV)
+		hap->vmax_mv = QPNP_HAP_VMAX_MIN_MV;
+	else if (hap->vmax_mv > QPNP_HAP_VMAX_MAX_MV)
+		hap->vmax_mv = QPNP_HAP_VMAX_MAX_MV;
 
 	rc = qpnp_hap_read_reg(hap, &reg, QPNP_HAP_VMAX_REG(hap->base));
 	if (rc < 0)
 		return rc;
 	reg &= QPNP_HAP_VMAX_MASK;
-	temp = hap->vmax_mv / hap->vtg_min;
+	temp = hap->vmax_mv / QPNP_HAP_VMAX_MIN_MV;
 	reg |= (temp << QPNP_HAP_VMAX_SHIFT);
 	rc = qpnp_hap_write_reg(hap, &reg, QPNP_HAP_VMAX_REG(hap->base));
 	if (rc)
@@ -1326,80 +1319,32 @@ static ssize_t qpnp_hap_ramp_test_data_show(struct device *dev,
 	}
 
 	return count;
-
 }
 
-static ssize_t qpnp_hap_vmax_mv_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct timed_output_dev *timed_dev = dev_get_drvdata(dev);
-	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
-					 timed_dev);
+int qpnp_hap_set(struct qpnp_hap *hap, int on);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vmax_mv);
-}
-
-static ssize_t qpnp_hap_vmax_mv_store(struct device *dev,
+static ssize_t qpnp_hap_vibrate_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
 	struct timed_output_dev *timed_dev = dev_get_drvdata(dev);
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
-	u32 data;
-	int rc;
+	int value = 1;
 
-	if (sscanf(buf, "%d", &data) != 1)
-		return -EINVAL;
+	sscanf(buf, "%d", &value);
+	value = (value > 1 ?
+				 1 : 0);
 
-	if (data < hap->vtg_min) {
-		pr_err("%s: mv %d not in range (%d - %d), using min.", __func__, data,
-				hap->vtg_min, hap->vtg_max);
-		data = hap->vtg_min;
-	} else if (data > hap->vtg_max) {
-		pr_err("%s: mv %d not in range (%d - %d), using max.", __func__, data,
-				hap->vtg_min, hap->vtg_max);
-		data = hap->vtg_max;
-	}
+	qpnp_hap_set(hap, value);
 
-	hap->vmax_mv = data;
-	rc = qpnp_hap_vmax_config(hap);
-	if (rc)
-		pr_info("qpnp: error while writing vibration control register\n");
-
-	return strnlen(buf, count);
+	return count;
 }
 
-static ssize_t qpnp_hap_min_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buf)
+/* sysfs show vibrate */
+static ssize_t qpnp_hap_vibrate_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
 {
-	struct timed_output_dev *timed_dev = dev_get_drvdata(dev);
-	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
-					 timed_dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_min);
-}
-
-static ssize_t qpnp_hap_max_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buf)
-{
-	struct timed_output_dev *timed_dev = dev_get_drvdata(dev);
-	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
-					 timed_dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_max);
-}
-
-static ssize_t qpnp_hap_default_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buf)
-{
-	struct timed_output_dev *timed_dev = dev_get_drvdata(dev);
-	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
-					 timed_dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
+	return snprintf(buf, PAGE_SIZE, "show for vibrate is not supported\n");
 }
 
 /* sysfs attributes */
@@ -1449,18 +1394,9 @@ static struct device_attribute qpnp_hap_attrs[] = {
 	__ATTR(min_max_test, (S_IRUGO | S_IWUSR | S_IWGRP),
 			qpnp_hap_min_max_test_data_show,
 			qpnp_hap_min_max_test_data_store),
-	__ATTR(vtg_level, (S_IRUGO | S_IWUSR | S_IWGRP),
-			qpnp_hap_vmax_mv_show,
-			qpnp_hap_vmax_mv_store),
-	__ATTR(vtg_min, S_IRUGO,
-			qpnp_hap_min_show,
-			NULL),
-	__ATTR(vtg_max, S_IRUGO,
-			qpnp_hap_max_show,
-			NULL),
-	__ATTR(vtg_default, S_IRUGO,
-			qpnp_hap_default_show,
-			NULL),
+	__ATTR(vibrate, (S_IRUGO | S_IWUSR | S_IWGRP),
+			qpnp_hap_vibrate_show,
+			qpnp_hap_vibrate_store),
 };
 
 static int calculate_lra_code(struct qpnp_hap *hap)
@@ -1651,14 +1587,12 @@ static void correct_auto_res_error(struct work_struct *auto_res_err_work)
 }
 
 /* set api for haptics */
-static int qpnp_hap_set(struct qpnp_hap *hap, int on)
+int qpnp_hap_set(struct qpnp_hap *hap, int on)
 {
 	int rc = 0;
 	u8 val = 0;
 	unsigned long timeout_ns = POLL_TIME_AUTO_RES_ERR_NS;
 	u32 back_emf_delay_us = hap->time_required_to_generate_back_emf_us;
-
-	mutex_lock(&hap->set_lock);
 
 	if (hap->play_mode == QPNP_HAP_PWM) {
 		if (on)
@@ -1686,10 +1620,8 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 				qpnp_hap_auto_res_enable(hap, 0);
 
 			rc = qpnp_hap_mod_enable(hap, on);
-			if (rc < 0) {
-				mutex_unlock(&hap->set_lock);
+			if (rc < 0)
 				return rc;
-			}
 
 			rc = qpnp_hap_play(hap, on);
 
@@ -1699,10 +1631,8 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 				usleep_range(back_emf_delay_us,
 							back_emf_delay_us + 1);
 				rc = qpnp_hap_auto_res_enable(hap, 1);
-				if (rc < 0) {
-					mutex_unlock(&hap->set_lock);
+				if (rc < 0)
 					return rc;
-				}
 			}
 			if (hap->act_type == QPNP_HAP_LRA &&
 						hap->correct_lra_drive_freq) {
@@ -1718,10 +1648,8 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 			}
 		} else {
 			rc = qpnp_hap_play(hap, on);
-			if (rc < 0) {
-				mutex_unlock(&hap->set_lock);
+			if (rc < 0)
 				return rc;
-			}
 
 			if (hap->act_type == QPNP_HAP_LRA &&
 						hap->correct_lra_drive_freq) {
@@ -1739,25 +1667,14 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 		}
 	}
 
-	mutex_unlock(&hap->set_lock);
 	return rc;
 }
 
-static void qpnp_timed_enable_worker(struct work_struct *work)
+/* enable interface from timed output class */
+static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 {
-	struct qpnp_hap *hap = container_of(work, struct qpnp_hap,
-					 td_work);
-	int value;
-
-	spin_lock(&hap->td_lock);
-	value = hap->td_value;
-	spin_unlock(&hap->td_lock);
-
-	/* Vibrator already disabled */
-	if (!value && !hap->state)
-		return;
-
-	flush_work(&hap->work);
+	struct qpnp_hap *hap = container_of(dev, struct qpnp_hap,
+					 timed_dev);
 
 	mutex_lock(&hap->lock);
 
@@ -1777,30 +1694,12 @@ static void qpnp_timed_enable_worker(struct work_struct *work)
 		value = (value > hap->timeout_ms ?
 				 hap->timeout_ms : value);
 		hap->state = 1;
-	}
-	mutex_unlock(&hap->lock);
-	if (hap->play_mode == QPNP_HAP_DIRECT)
-		qpnp_hap_set(hap, hap->state);
-	else
-		schedule_work(&hap->work);
-
-	if (value)
 		hrtimer_start(&hap->hap_timer,
 			      ktime_set(value / 1000, (value % 1000) * 1000000),
 			      HRTIMER_MODE_REL);
-}
-
-/* enable interface from timed output class */
-static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
-{
-	struct qpnp_hap *hap = container_of(dev, struct qpnp_hap,
-					 timed_dev);
-
-	spin_lock(&hap->td_lock);
-	hap->td_value = value;
-	spin_unlock(&hap->td_lock);
-
-	schedule_work(&hap->td_work);
+	}
+	mutex_unlock(&hap->lock);
+	schedule_work(&hap->work);
 }
 
 /* play pwm bytes */
@@ -1918,10 +1817,6 @@ static enum hrtimer_restart qpnp_hap_timer(struct hrtimer *timer)
 {
 	struct qpnp_hap *hap = container_of(timer, struct qpnp_hap,
 							 hap_timer);
-
-	/* Vibrator already disabled */
-	if (!hap->state)
-		return HRTIMER_NORESTART;
 
 	hap->state = 0;
 	schedule_work(&hap->work);
@@ -2398,33 +2293,6 @@ static int qpnp_hap_parse_dt(struct qpnp_hap *hap)
 		return rc;
 	}
 
-	hap->vtg_min = QPNP_HAP_VMAX_MIN_MV;
-	rc = of_property_read_u32(spmi->dev.of_node,
-			"qcom,hap-vtg-min-mv", &temp);
-	if (!rc) {
-		hap->vtg_min = temp;
-	} else if (rc != -EINVAL) {
-		dev_err(&spmi->dev, "Unable to read vtg_min\n");
-		return rc;
-	}
-
-	hap->vtg_max = QPNP_HAP_VMAX_MAX_MV;
-	rc = of_property_read_u32(spmi->dev.of_node,
-			"qcom,hap-vtg-max-mv", &temp);
-	if (!rc) {
-		hap->vtg_max = temp;
-	} else if (rc != -EINVAL) {
-		dev_err(&spmi->dev, "Unable to read vtg_max\n");
-		return rc;
-	}
-
-	if (hap->vmax_mv < hap->vtg_min)
-		hap->vmax_mv = hap->vtg_min;
-	else if (hap->vmax_mv > hap->vtg_max)
-		hap->vmax_mv = hap->vtg_max;
-
-	hap->vtg_default = hap->vmax_mv;
-
 	hap->ilim_ma = QPNP_HAP_ILIM_MIN_MV;
 	rc = of_property_read_u32(spmi->dev.of_node,
 			"qcom,ilim-ma", &temp);
@@ -2561,13 +2429,9 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 
 	mutex_init(&hap->lock);
 	mutex_init(&hap->wf_lock);
-	mutex_init(&hap->set_lock);
-	spin_lock_init(&hap->td_lock);
-
 	INIT_WORK(&hap->work, qpnp_hap_worker);
 	INIT_DELAYED_WORK(&hap->sc_work, qpnp_handle_sc_irq);
 	init_completion(&hap->completion);
-	INIT_WORK(&hap->td_work, qpnp_timed_enable_worker);
 
 	hrtimer_init(&hap->hap_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hap->hap_timer.function = qpnp_hap_timer;
