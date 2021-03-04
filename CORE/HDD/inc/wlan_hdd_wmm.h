fb0cd9a4f904788c15c0de5e8bd1bd19bb621682
Shreyas Narayan <shrena@codeaurora.org>
2021-03-03 22:09:31 -0800

    Revert "prima: ndo_select_queue support for 4.19 kernel"
    This reverts commit d1a4989c3e3ddd3e24d6972964bbda5f1a41d870.
    
    Change-Id: I67589b2746c8d989e6ad9b00308c4287f1d66b9c

--- CORE/HDD/inc/wlan_hdd_wmm.h
+++ CORE/HDD/inc/wlan_hdd_wmm.h
@@ -264,23 +264,14 @@ v_U16_t hdd_wmm_select_queue(struct net_device * dev, struct sk_buff *skb);
   @return         : Qdisc queue index
   ===========================================================================*/
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0))
-uint16_t hdd_hostapd_select_queue(struct net_device *dev, struct sk_buff *skb,
-				  struct net_device *sb_dev,
-				  select_queue_fallback_t fallbac);
-
-#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
-uint16_t hdd_hostapd_select_queue(struct net_device *dev, struct sk_buff *skb,
-				  void *accel_priv,
-				  select_queue_fallback_t fallbac);
-
-#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0))
-uint16_t hdd_hostapd_select_queue(struct net_device *dev, struct sk_buff *skb,
-				  void *accel_priv);
-
-#else
-uint16_t hdd_hostapd_select_queue(struct net_device *dev, struct sk_buff *skb);
+v_U16_t hdd_hostapd_select_queue(struct net_device * dev, struct sk_buff *skb
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0))
+                                 , void *accel_priv
 #endif
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0))
+                                 , select_queue_fallback_t fallbac
+#endif
+);
 
 
 /**============================================================================
