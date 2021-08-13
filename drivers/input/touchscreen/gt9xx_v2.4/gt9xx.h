ec70f92995fc1ec8cf107f09d15dfd70d62ae103
Roman Rihter <teledurak@gmail.com>
2021-08-13 21:59:00 +0300

    m1721: Fix Goodix touchscreen

--- drivers/input/touchscreen/gt9xx_v2.4/gt9xx.h
+++ drivers/input/touchscreen/gt9xx_v2.4/gt9xx.h
@@ -52,13 +52,13 @@
 #define GTP_CUSTOM_CFG        1
 #define GTP_CHANGE_X2Y        0
 #define GTP_DRIVER_SEND_CFG   1
-#define GTP_HAVE_TOUCH_KEY    1
+#define GTP_HAVE_TOUCH_KEY    0
 #define GTP_POWER_CTRL_SLEEP  0
 #define GTP_ICS_SLOT_REPORT   0
 
 #define CONFIG_GTP_GLOVE_MODE 1
-#define GTP_AUTO_UPDATE      1
-#define GTP_HEADER_FW_UPDATE  1
+#define GTP_AUTO_UPDATE       0
+#define GTP_HEADER_FW_UPDATE  0
 #define GTP_AUTO_UPDATE_CFG   0
 
 #define GTP_COMPATIBLE_MODE   0
