90ab727e4ed00ef752f87924c93a134a224b77af
Roman Rihter <teledurak@gmail.com>
2021-12-18 18:55:23 +0300

    m1721: fs/pstore: import Meizu changes

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -262,11 +262,27 @@ ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 	return ret;
 }
 
+#ifdef CONFIG_MACH_MEIZU_M1721
+static void *memcpy_pstore(void *dest, const void *src, size_t count)
+{
+	char *tmp = dest;
+	const char *s = src;
+
+	while (count--)
+		*tmp++ = *s++;
+	return dest;
+}
+#endif
+
 static void notrace persistent_ram_update(struct persistent_ram_zone *prz,
 	const void *s, unsigned int start, unsigned int count)
 {
 	struct persistent_ram_buffer *buffer = prz->buffer;
+#ifdef CONFIG_MACH_MEIZU_M1721
+	memcpy_pstore(buffer->data + start, s, count);
+#else
 	memcpy_toio(buffer->data + start, s, count);
+#endif
 	persistent_ram_update_ecc(prz, start, count);
 }
 
