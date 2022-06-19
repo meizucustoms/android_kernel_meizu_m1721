300a92a4e47cf7416538337e6a6f6f962623c3c1
Roman Rihter <teledurak@gmail.com>
2022-06-19 21:48:07 +0300

    erofs: remove STATX usage

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -337,18 +337,10 @@ struct inode *erofs_iget(struct super_block *sb,
 	return inode;
 }
 
-int erofs_getattr(const struct path *path, struct kstat *stat,
-		  u32 request_mask, unsigned int query_flags)
+int erofs_getattr(struct vfsmount *mnt, 
+	struct dentry *dentry, struct kstat *stat)
 {
-	struct inode *const inode = d_inode(path->dentry);
-
-	if (erofs_inode_is_data_compressed(EROFS_I(inode)->datalayout))
-		stat->attributes |= STATX_ATTR_COMPRESSED;
-
-	stat->attributes |= STATX_ATTR_IMMUTABLE;
-	stat->attributes_mask |= (STATX_ATTR_COMPRESSED |
-				  STATX_ATTR_IMMUTABLE);
-
+	struct inode *const inode = d_inode(dentry);
 	generic_fillattr(inode, stat);
 	return 0;
 }
