6c5f0a11c57d8fc3502bcafa4f9d789344b4058e
Roman Rihter <teledurak@gmail.com>
2022-06-20 03:41:37 +0300

    erofs: zdata: use right size for kvmalloc

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -863,7 +863,7 @@ static int z_erofs_decompress_pcluster(struct super_block *sb,
 		if (nr_pages > Z_EROFS_VMAP_GLOBAL_PAGES)
 			gfp_flags |= __GFP_NOFAIL;
 
-		pages = kvmalloc(sizeof(struct page *), gfp_flags);
+		pages = kvmalloc(sizeof(struct page *) * nr_pages, gfp_flags);
 
 		/* fallback to global pagemap for the lowmem scenario */
 		if (!pages) {
