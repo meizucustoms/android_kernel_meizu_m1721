ee556f021c744ae4483e0a9c11fae9cf823e65e4
Roman Rihter <teledurak@gmail.com>
2022-06-21 01:33:29 +0300

    Revert "simple_lmk: Use vmpressure notifier to trigger kills"
    This reverts commit 5805a7d8bb3bf0386b88da1d371b02108f5781de.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -48,6 +48,7 @@
 #include <linux/printk.h>
 #include <linux/dax.h>
 #include <linux/psi.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -3345,6 +3346,7 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 		unsigned long nr_reclaimed = sc.nr_reclaimed;
 		bool raise_priority = true;
 
+		simple_lmk_decide_reclaim(sc.priority);
 		sc.reclaim_idx = classzone_idx;
 
 		/*
